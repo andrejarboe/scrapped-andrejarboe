98236694f667589e5440be0eb19b4fc8295b9a11
andrejarboe <andrejarboe@gmail.com>
2018-10-02 19:43:52 -0400

    put cms in correct folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   // siteMetadata: {
   //   title: 'Gatsby Default Starter',
   // },
-  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-netlify-cms"]
+  plugins: [
+    {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: "src",
+          path: `${__dirname}/src/`
+        }
+      },
+      `gatsby-plugin-react-helmet`, 
+      `gatsby-plugin-netlify-cms`
+    ]
 };
