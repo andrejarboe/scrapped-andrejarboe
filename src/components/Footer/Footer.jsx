b187ad87bae40874fa956ccdb1861b1972ef2f0b
andrejarboe <andrejarboe@gmail.com>
2018-10-02 15:12:48 -0400

    footer css grid working

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,70 +1,121 @@
 import React, { Component } from "react";
-
 import { Link } from "gatsby";
+
+import "./Footer.css";
 
 export default class Footer extends Component {
   render() {
     return (
       <footer className="page-footer">
         <div className="container">
-          <div className="row">
-            <div className="col l6 s12">
-              <h5 className="white-text">Andre Jarboe</h5>
-              <p className="grey-text text-lighten-4">
-                You can use rows and columns here to organize your footer
-                content.
-              </p>
+          <div className="footer-content row">
+            <div className="title">
+            <Link
+                  exact
+                  to="/"
+                  className="grey-text text-lighten-3"
+                >
+                  Andre Jarboe
+                </Link>
             </div>
-            <div className="col l4 offset-l2 s12">
-              <h5 className="white-text">Site Map</h5>
-              <ul>
-                <li>
-                  <Link
-                    exact
-                    to="/"
-                    className="grey-text text-lighten-3"
-                    activeClassName="activeFooter"
-                  >
-                    Home
-                  </Link>
-                </li>
-                <li>
-                <Link exact to="/about" className="grey-text text-lighten-3"
-                    activeClassName="activeFooter">
+            <div className="social">
+            <a href="https://www.instagram.com/andrejarboeii/" target="_blank">
+        <i className="fab fa-instagram " />
+      </a>
+      <a href="https://www.facebook.com/andrejarboeii/" target="_blank">
+        <i className="fab fa-facebook " />
+      </a>
+      <a href="https://www.instagram.com/andrejarboeii/" target="_blank">
+        <i className="fab fa-twitter " />
+      </a>
+      <a href="https://www.instagram.com/andrejarboeii/" target="_blank">
+        <i className="fab fa-imdb" />
+      </a>
+      <a href="https://www.instagram.com/andrejarboeii/" target="_blank">
+        <i className="fab fa-youtube " />
+      </a>
+      <a href="https://www.instagram.com/andrejarboeii/" target="_blank">
+        <i className="fab fa-instagram " />
+      </a>
+            </div>
+            <div className="site-map">
+              <li>
+                <Link
+                  exact
+                  to="/"
+                  className="grey-text text-lighten-3"
+                  activeClassName="activeFooter"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  exact
+                  to="/about"
+                  className="grey-text text-lighten-3"
+                  activeClassName="activeFooter"
+                >
                   About
                 </Link>
               </li>
+
               <li>
-                <Link exact to="/reels" className="grey-text text-lighten-3"
-                    activeClassName="activeFooter">
+                <Link
+                  exact
+                  to="/reels"
+                  className="grey-text text-lighten-3"
+                  activeClassName="activeFooter"
+                >
                   Reels
                 </Link>
               </li>
+
               <li>
-                <Link exact to="/headshots" className="grey-text text-lighten-3"
-                    activeClassName="activeFooter">
+                <Link
+                  exact
+                  to="/headshots"
+                  className="grey-text text-lighten-3"
+                  activeClassName="activeFooter"
+                >
                   Headshots
                 </Link>
               </li>
               <li>
-                <Link exact to="/resume" className="grey-text text-lighten-3"
-                    activeClassName="activeFooter">
+                <Link
+                  exact
+                  to="/resume"
+                  className="grey-text text-lighten-3"
+                  activeClassName="activeFooter"
+                >
                   Resume
                 </Link>
               </li>
               <li>
-                <Link exact to="/pictures" className="grey-text text-lighten-3"
-                    activeClassName="activeFooter">
+                <Link
+                  exact
+                  to="/pictures"
+                  className="grey-text text-lighten-3"
+                  activeClassName="activeFooter"
+                >
                   Pictures
                 </Link>
               </li>
               <li>
-                <Link exact to="/blog" className="grey-text text-lighten-3"
-                    activeClassName="activeFooter">
+                <Link
+                  exact
+                  to="/blog"
+                  className="grey-text text-lighten-3"
+                  activeClassName="activeFooter"
+                >
                   Blog
                 </Link>
               </li>
-              </ul>
+            </div>
+            <div className="design">
+              <a href="https://www.jarboestudios.com/" target="_blank">
+                Designed and coded by Jarboe studios
+              </a>
             </div>
           </div>
         </div>
