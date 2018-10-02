0f0a63205d49ab2041d4dda02a5bd79a3e4496e1
andrejarboe <andrejarboe@gmail.com>
2018-10-02 17:29:39 -0400

    fixed footer for mobile lol

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 module.exports = {
-    // siteMetadata: {
-    //   title: 'Gatsby Default Starter',
-    // },
-    plugins: [    'gatsby-plugin-netlify-cms',
-],
-}
+  // siteMetadata: {
+  //   title: 'Gatsby Default Starter',
+  // },
+  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-netlify-cms"]
+};
