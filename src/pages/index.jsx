39e9db6016c399acaeb791df814e9debda89c8c9
andrejarboe <andrejarboe@gmail.com>
2018-10-10 20:15:25 -0400

    add headshots to index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,29 +1,38 @@
 import React from "react";
 import Layout from "../components/layout";
 
-export default () => (
+export default function IndexPage({ data }) {
+
+  return (
+
   <Layout>
     <div className="container">
       <div className="row">
-        <div className="col s12 m4">
-          <img
-            src="https://images.unsplash.com/photo-1538035272021-5cfc10936ac0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d012b839c989948d08ce7f5be767a8d1&auto=format&fit=crop&w=657&q=80"
-            alt=""
-          />
-        </div>
-        <div className="col s12 m4">
-          <img
-            src="https://images.unsplash.com/photo-1538035272021-5cfc10936ac0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d012b839c989948d08ce7f5be767a8d1&auto=format&fit=crop&w=657&q=80"
-            alt=""
-          />
-        </div>
-        <div className="col s12 m4">
-          <img
-            src="https://images.unsplash.com/photo-1538035272021-5cfc10936ac0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d012b839c989948d08ce7f5be767a8d1&auto=format&fit=crop&w=657&q=80"
-            alt=""
-          />
-        </div>
+          {data.markdownRemark.frontmatter.headshots.map(image => {
+            return (
+              <div className="col s12 m4">
+                <img key={image.path} src={image.path} alt="" />
+              </div>
+            )
+          })}
+
+
+
       </div>
     </div>
   </Layout>
-);
+  )
+}
+
+export const query = graphql`
+  query indexQuery {
+    markdownRemark(frontmatter: { layout: { eq: "home" } }) {
+      frontmatter {
+        layout
+        headshots {
+          path
+        }
+      }
+    }
+  }
+`
